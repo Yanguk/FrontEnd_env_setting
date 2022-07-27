a1dd827a1f76f759dde73fc36601200ccb7e4a8d
Yanguk <heo.yanguk@gmail.com>
2022-07-27 12:24:31 +0900

    node_modules ignore_2

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
   mode,
   entry: './src/index.tsx',
