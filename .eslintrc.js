96e48d3980394733056931d9ff1628c40237321b
Yanguk <heo.yanguk@gmail.com>
2022-07-28 14:56:00 +0900

    eslint airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        // parser: 'babel-ts',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- src/app.tsx
+++ src/app.tsx
@@ -3,21 +3,33 @@ import React from 'react';
 function Test() {
   return (
     <div>
+<<<<<<< HEAD:src/app.tsx
       하이룽 테스트 입다다
       하이룽 테스트 입다다
       하이룽 테스트 입다다
       하이룽 테스트 입다다
+=======
+      하이룽 테스트 입다다 하이룽 테스트 입다다 하이룽 테스트 입다다 하이룽
+      테스트 입다다
+>>>>>>> 3d77e1a (eslint airbnb):src/App.tsx
     </div>
-  )
+  );
 }
 
 function App(): JSX.Element {
   return (
     <>
+<<<<<<< HEAD:src/app.tsx
     <Test/>
     <div>
       <h1>리액트 jsx 환경 까지 구성완료~ 타입스크립트로</h1>
     </div>
+=======
+      <Test />
+      <div>
+        <h1>리액트 jsx 환경 까지 구성완료~ 타입스크립트로</h1>
+      </div>
+>>>>>>> 3d77e1a (eslint airbnb):src/App.tsx
     </>
   );
 }
