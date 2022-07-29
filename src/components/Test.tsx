14ae24fbd7ecfbc0071d37edf8eb27490b531279
Yanguk <heo.yanguk@gmail.com>
2022-07-29 20:15:31 +0900

    react전용 hot-loading 완료

---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div style={{ backgroundColor: 'blue' }}>
+      Test 핫로딩 설정완료~~ 리액트는 따로 설정해줘야 하는구나
+    </div>
+  );
+}
+
+export default Test;
